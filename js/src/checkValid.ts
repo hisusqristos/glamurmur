4d3e20c05b720a4a26484d53883e8a287d0c6f2c
blasteroid <varyadaser@gmail.com>
2023-05-30 16:44:02 +0400

    :soap: get rid of red underlines

--- js/src/checkValid.ts
+++ js/src/checkValid.ts
@@ -1,4 +1,4 @@
 // ELEGANTLY filter the items that exit in the second arrays
-var checkValid = (arr1: [], arr2: []) => {
+var checkValid = (arr1: number[], arr2: number[]) => {
     return arr1.filter(item => !arr2.includes(item));
 }
